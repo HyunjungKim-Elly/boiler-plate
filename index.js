95c119df967e0654951eca754486df960d68436c
HyunjungKim-Elly <ujujo@naver.com>
2020-10-15 14:40:21 +0900

    security

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser")
+const config = require('./config/key');
+const { User } = require("./models/User")
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://ellykim:elly1004@cluster0.yohze.mongodb.net/test?retryWrites=true&w=majority', {
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("Hello World!"))
+
+app.post('/register', (req, res) => {
+    //if I get the information for sign-up, will put into database
+    
+  
+    const user = new User(req.body)
+
+    
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
